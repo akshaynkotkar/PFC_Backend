77e1c88f2f168837b6e53b74234ab8ff9a9f9ddc
siddhiamale30 <ssamale3010@gmail.com>
2023-04-17 21:29:06 +0530

    testing github

---
+++ admin/index.php
@@ -0,0 +1,17 @@
+
+<?php
+    include('includes/header.php');
+?>
+<div>
+    <div>
+        <div>
+            <h1>Hey...Siddhi</h1>
+        </div>
+    </div>
+
+</div>
+    
+
+<?php
+    include('includes/footer.php');
+?>
